767db308232e584727fcf90caf4583787f8ce92a
VincenzoFerraro0 <ferrarov0@gmail.com>
2025-02-06 13:47:30 +0100

    step 2

---
+++ js/hobbie.js
@@ -0,0 +1,11 @@
+/*
+2 - hobbies.js
+1. Crea una funzione che accetta tre parametri: hobbyOne, hobbyTwo, hobbyThree. La funzione dovrebbe restituire  un array di stringhe contenenti gli hobby.
+2. Esporta la funzione dal file.
+*/
+
+function createHobbie (hobbieOne,hobbieTwo,hobbieThree ) {
+    return [ hobbieOne, hobbieTwo,hobbieThree ]
+}
+
+export default createHobbie
